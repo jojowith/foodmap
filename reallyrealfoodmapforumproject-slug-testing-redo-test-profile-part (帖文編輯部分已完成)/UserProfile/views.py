d22d432a61bc7bf0e7ac4de8e79664480b58cb92
jojowith <wdoesthefox@gmail.com>
2023-07-31 17:31:46 +0800

    hello

---
+++ reallyrealfoodmapforumproject-slug-testing-redo-test-profile-part (帖文編輯部分已完成)/UserProfile/views.py
@@ -0,0 +1,58 @@
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from Account.forms import SignUpForm
+from django.contrib import messages
+from django.shortcuts import render,redirect
+from .models import Profile
+
+
+# Create your views here.
+
+
+def profile_list_view(request):
+    #profiles = Profile.objects.all()
+     profiles = Profile.objects.exclude(user=request.user)
+     return render(request, 'profile/UserProfileList.html', {"profiles": profiles})
+
+
+#def profile_list_view_non_signup_signin(request):
+   # non_signup_signin_profiles = Profile.objects.all()
+    #return render(request, 'profile/nonSigninSignupUserProfileList.html',
+                  #{"non_signup_signin_profiles": non_signup_signin_profiles})
+
+
+
+def profile_not_own_view(request, slug):
+    notOwnProfile = Profile.objects.get(slug=slug)
+    #context = {
+    #    'notOwnProfile': notOwnProfile
+    #}
+
+    if request.method == "POST":
+        current_user_profile = request.user.profile
+
+        action = request.POST['follow']
+
+        if action == "unfollow":
+            current_user_profile.follows.remove(notOwnProfile)
+        elif action == "follow":
+            current_user_profile.follows.add(notOwnProfile)
+
+        current_user_profile.save()
+    return render(request, 'profile/notOwnProfile.html', {"notOwnProfile": notOwnProfile})
+
+def update_user(request, slug):
+    notOwnProfile = Profile.objects.get(slug=slug)
+    if request.user.is_authenticated:
+        current_user= User.objects.get(id=request.user.id)
+        form= SignUpForm(request.POST or None, instance=current_user)
+        if form.is_valid():
+            form.save()
+            login(request, current_user)
+            messages.success(request, ("你的個人檔案已更新"))
+            return  redirect('notownprofile')
+
+        return render(request, "profile/update_user.html",{'form':form,"notOwnProfile": notOwnProfile})
+    else:
+        messages.success(request, ("你必須登入才能觀看此頁面"))
+        return redirect('home')
